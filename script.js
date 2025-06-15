c7f82d3a1e7e2829fb87841b84b6c3d163314267
Nehe4 <ojetadenehemiah@gmail.com>
2025-06-15 15:24:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const Display = document
-.querySelector('.input');
+const Display = document.querySelector('.input');
 
 function button(btn) {
- Display.value += btn
+  Display.value += btn;
 }
 
 function Eraze() {
-  Display.value ='';
+  Display.value = '';
 }
 
+function backspace() {
+  Display.value = Display.value.slice(0, -1);
+}
 
 function cal() {
-try {
-Display.value = eval(Display.value)
-}
- catch (errors) {
- Display.value = 'ERROR';
-}
+  try {
+    Display.value = eval(Display.value);
+  } catch (errors) {
+    Display.value = 'ERROR';
+  }
 }
